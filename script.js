e007bd727208522ab0a04c0884cbb1e962571ff5
jaredlomangino <jared.lomangino@gmail.com>
2020-02-13 15:17:38 -0500

    Added display information for specific cocktails

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const submit = document.getElementById("submit");
 const search = document.getElementById("search");
 const random = document.getElementById("random");
 const cocktailElement = document.getElementById("cocktails");
+const singleCocktail = document.getElementById("cocktail-element");
 const resultHeading = document.getElementById("result-heading");
 const errorMessage = document.getElementById("error-message");
 
@@ -31,7 +32,7 @@ function searchForCocktail(e) {
                 `
                 <div class="cocktail">
                     <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}"/>
-                    <div class="cocktail-info" data-cocktailID="${cocktail.idCocktail}">
+                    <div class="cocktail-info" data-cocktailID="${cocktail.idDrink}">
                         <h3>${cocktail.strDrink}</h3>
                     </div>
                 </div>
@@ -46,4 +47,74 @@ function searchForCocktail(e) {
   }
 }
 
+function getCocktailbyID(cocktailID) {
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailID}`
+  )
+    .then(res => res.json())
+    .then(data => {
+      const cocktail = data.drinks[0];
+
+      addCocktailToDOM(cocktail);
+    });
+}
+
+function getRandomCocktail() {
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+      const cocktail = data.drinks[0];
+
+      addCocktailToDOM(cocktail);
+    });
+}
+
+function addCocktailToDOM(cocktail) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (cocktail[`strIngredient${i}`]) {
+      ingredients.push(
+        `${cocktail[`strIngredient${i}`]} - ${cocktail[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleCocktail.innerHTML = `
+    <div class="single cocktail">
+      <h1>${cocktail.strDrink}</h1>
+      <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}/>
+      <div class="single-cocktail-info">
+        ${cocktail.strGlass ? `<p>Glass Type: ${cocktail.strGlass}</p>` : ""}
+      </div>
+      <div class="main">
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join("")}
+        </ul>
+        <p>${cocktail.strInstructions}</p>
+      </div>
+    </div>
+  `;
+}
+
 submit.addEventListener("submit", searchForCocktail);
+
+random.addEventListener("click", getRandomCocktail);
+
+cocktailElement.addEventListener("click", e => {
+  const cocktailInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains("cocktail-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (cocktailInfo) {
+    const cocktailID = cocktailInfo.getAttribute("data-cocktailID");
+    getCocktailbyID(cocktailID);
+  }
+});
